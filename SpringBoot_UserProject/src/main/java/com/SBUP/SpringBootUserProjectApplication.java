32e9e439a5997d533d1c8f5df0b4f0338976c61d
Dinesh <dineshkumarnadar01@gmail.com>
2025-04-11 16:50:19 +0530

    Main Class

---
+++ SpringBoot_UserProject/src/main/java/com/SBUP/SpringBootUserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SBUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserProjectApplication.class, args);
+	}
+
+}
