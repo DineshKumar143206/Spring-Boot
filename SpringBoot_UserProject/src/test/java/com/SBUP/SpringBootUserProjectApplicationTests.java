dff29ceb90957b77ae5e3105e773cd4c188a19a8
Dinesh <dineshkumarnadar01@gmail.com>
2025-04-11 16:51:11 +0530

    other details

---
+++ SpringBoot_UserProject/src/test/java/com/SBUP/SpringBootUserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SBUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
